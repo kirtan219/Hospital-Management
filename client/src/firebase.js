6a667f3509a642f917e649006c867d99177c7a90
kirtan219 <kirtan2193@gmail.com>
2025-05-06 21:58:06 +0530

    six

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,22 +1,49 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
-import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyARYo4nMyB8CA4KSo_n5Bss_OU9s4mD7Bw",
-  authDomain: "hospital-mangement-b23b6.firebaseapp.com",
-  projectId: "hospital-mangement-b23b6",
-  storageBucket: "hospital-mangement-b23b6.firebasestorage.app",
-  messagingSenderId: "377136155693",
-  appId: "1:377136155693:web:f121b96e04a8505a8cfca4",
-  measurementId: "G-GHMKBV1RPT"
+// Mock Firebase implementation
+const app = {
+  name: 'firebase-app-mock'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+export const auth = {
+  currentUser: null,
+  onAuthStateChanged: (callback) => {
+    callback(null);
+    return () => {};
+  },
+  signInWithEmailAndPassword: async () => {
+    throw new Error('Firebase auth is not available');
+  },
+  createUserWithEmailAndPassword: async () => {
+    throw new Error('Firebase auth is not available');
+  },
+  signOut: async () => {}
+};
+
+export const db = {
+  collection: () => ({
+    add: async () => ({}),
+    doc: () => ({
+      get: async () => ({
+        exists: false,
+        data: () => ({}),
+        id: ''
+      }),
+      set: async () => {},
+      update: async () => {},
+      delete: async () => {}
+    }),
+    where: () => ({
+      get: async () => ({
+        docs: [],
+        empty: true
+      }),
+      onSnapshot: () => () => {}
+    }),
+    onSnapshot: () => () => {}
+  })
+};
+
+export const analytics = {
+  logEvent: () => {}
+};
 
 export default app; 
